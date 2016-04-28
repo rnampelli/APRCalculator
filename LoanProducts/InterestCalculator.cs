42fdec94ff7126dc7c0326664f6eb79cfb9e5bfa
rnampelli <rohitnampelli@gmail.com>
2016-04-27 23:55:29 -0400

    Interest Calculator

---
+++ LoanProducts/InterestCalculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanProducts
+{
+    class InterestCalculator
+    {
+        protected Loan loan { get; set; }
+        protected decimal interest { get; set; }
+        public InterestCalculator(Loan Loan)
+        {
+            this.loan = Loan;
+        }
+
+        public decimal CalculateInterest(DateTime FromDate, DateTime ToDate, decimal Amount) {
+            decimal result = 0.00m;
+            decimal AbsoluteAmount = System.Math.Abs(Amount);
+            int period = ToDate.Date.Subtract(FromDate.Date).Days;
+            decimal tierAmount = 0.00m, residue = AbsoluteAmount;
+            if (period == 0 || AbsoluteAmount == 0)
+                return result;
+            foreach (InterestTier tier in this.loan.InterestTiers.Where(x=> x.MinAmount < AbsoluteAmount).OrderByDescending(x => x.MaxAmount)) {
+                tierAmount = residue > tier.MinAmount ? residue - tier.MinAmount : 0;
+                result = result + (tierAmount * (tier.Value / 100.00m) / 365 * period);
+                residue = residue - tierAmount;
+            }
+            return result;
+        }
+    }
+}
--- LoanProducts/InterestRate.cs
+++ LoanProducts/InterestRate.cs
@@ -9,5 +9,11 @@ namespace LoanProducts {
         public decimal MinAmount { get; set; }
         public decimal MaxAmount { get; set; }
         public decimal Value { get; set; }
+
+        public InterestTier(decimal MinAmount, decimal MaxAmount, decimal Value) {
+            this.MinAmount = MinAmount;
+            this.MaxAmount = MaxAmount;
+            this.Value = Value;
+        }
     }
 }
